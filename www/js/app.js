d04ab83f58aa511f0fdbd2e7970dc6ecbab7c09d
amilonurb <brunolima.18@outlook.com>
2019-05-09 00:25:39 -0300

    Ajustes

--- www/js/app.js
+++ www/js/app.js
@@ -1,14 +1,19 @@
-﻿$('.collection').on('click', '.collection-item', function() {
-    var $badge = $('.badge', this);
-    if ($badge.length === 0) {
-        $badge = $('<span class="badge brown-text">0</span>').appendTo(this);
-    }
+﻿$('.collection')
+    .on('click', '.collection-item', function() {
+        var $badge = $('.badge', this);
+        if ($badge.length === 0) {
+            $badge = $('<span class="badge brown-text">0</span>').appendTo(this);
+        }
 
-    $badge.text(parseInt($badge.text()) + 1);
+        $badge.text(parseInt($badge.text()) + 1);
 
-    var nomeProduto = this.firstChild.textContent;
-    Materialize.toast(nomeProduto + ' adicionado', 1000);
-});
+        var nomeProduto = this.firstChild.textContent;
+        Materialize.toast(nomeProduto + ' adicionado', 1000);
+    })
+    .on('click', '.badge', function() {
+        $(this).remove();
+        return false;
+    });
 
 $('.modal-trigger').leanModal();
 
@@ -21,4 +26,9 @@ $('#confirmar').on('click', function() {
     });
 
     $('#resumo').empty().text(texto);
+});
+
+$('.acao-limpar').on('click', function() {
+    $('#numero-mesa').val('');
+    $('.badge').remove();
 });
